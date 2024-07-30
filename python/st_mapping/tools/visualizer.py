a5509d1c988bb45741b272da017d06ce24bf0cc5
Luca Lobefaro <luca.lobefaro95@gmail.com>
2024-07-30 11:39:44 +0200

    Some GUI improvement

--- python/st_mapping/tools/visualizer.py
+++ python/st_mapping/tools/visualizer.py
@@ -27,6 +27,7 @@ import cv2
 
 import numpy as np
 
+# OLD PARAMETERS: TODO remove them
 YELLOW = np.array([1, 0.706, 0])
 RED = [1.0, 0.0, 0.0]
 BLACK = np.array([0, 0, 0])
@@ -34,6 +35,8 @@ WHITE = np.array([1.0, 1.0, 1.0])
 BLUE = np.array([0.4, 0.5, 0.9])
 ARB = np.array([0.1, 0.5, 0.6])
 SPHERE_SIZE = 0.20
+
+BACKGROUND_COLOR = [0.8470, 0.8588, 0.8863]
 
 
 class StubVisualizer(ABC):
@@ -47,19 +50,48 @@ class StubVisualizer(ABC):
         pass
 
 
+def start_pause_callback():
+    button_name = "PAUSE" if PangolinoVisualizer.play_mode else "START"
+    if psim.Button(button_name) or psim.IsKeyPressed(psim.ImGuiKey_Space):
+        PangolinoVisualizer.play_mode = not PangolinoVisualizer.play_mode
+
+
+def next_frame_callback():
+    if psim.Button("NEXT FRAME") or psim.IsKeyPressed(psim.ImGuiKey_N):
+        PangolinoVisualizer.block_execution = not PangolinoVisualizer.block_execution
+
+
+def quit_callback():
+    if (
+        psim.Button("QUIT")
+        or psim.IsKeyPressed(psim.ImGuiKey_Escape)
+        or psim.IsKeyPressed(psim.ImGuiKey_Q)
+    ):
+        PangolinoVisualizer.quit()
+        os._exit(0)
+
+
+def main_gui_callback():
+    start_pause_callback()
+    if not PangolinoVisualizer.play_mode:
+        psim.SameLine()
+        next_frame_callback()
+    psim.Separator()
+    quit_callback()
+
+
 class PangolinoVisualizer(StubVisualizer):
+    # Static parameteters
+    play_mode: bool = False
+    block_execution: bool = True
+
     def __init__(self):
         # Initialize visualizer
         self._initialize_visualizer()
         self._img_window = cv2.namedWindow("Image Stream")
 
-        # Initialize GUI controls
-        ps._block_execution = True
-        ps._play_mode = False
-
         # Initilize attributes
-        self._next_cam_number = 0
-        self._cameras = []
+        self._camera_poses = []
 
     def update(self, pose, rgb_img, frame_pcd):
         # Visualize image
@@ -75,60 +107,48 @@ class PangolinoVisualizer(StubVisualizer):
         # Visualization loop
         self._update_visualizer()
 
-    def quit(self):
-        ps.unshow()
-        cv2.destroyAllWindows()
-        pass
-
     def _initialize_visualizer(self):
+        ps.set_program_name("Spatio Temporal Mapping Visualizer")
         ps.init()
+        ps.set_background_color(BACKGROUND_COLOR)
+        ps.set_verbosity(0)
         ps.set_ground_plane_mode("none")
-        ps.set_user_callback(PangolinoVisualizer._gui_callback)
+        ps.set_user_callback(main_gui_callback)
+        ps.set_build_default_gui_panels(False)
 
     def _update_camera_pose(self, pose):
-        # Create new camera pose
-        camera_params = ps.CameraParameters(
-            ps.CameraIntrinsics(fov_vertical_deg=30, aspect=2),
-            ps.CameraExtrinsics(mat=np.linalg.inv(pose)),
-        )
-        new_cam = ps.register_camera_view(f"cam {self._next_cam_number}", camera_params)
-        new_cam.set_widget_color(RED)
-        self._next_cam_number += 1
-        self._cameras.append(new_cam)
+        pass
 
-        # Resize and re-color previous camera
-        if len(self._cameras) > 1:
-            self._cameras[-2].set_widget_color(BLACK)
-            self._cameras[-2].set_widget_focal_length(0.02)
+    # Create new camera pose
+    # camera_params = ps.CameraParameters(
+    #     ps.CameraIntrinsics(fov_vertical_deg=30, aspect=2),
+    #     ps.CameraExtrinsics(mat=np.linalg.inv(pose)),
+    # )
+    # new_cam = ps.register_camera_view(f"cam {self._next_cam_number}", camera_params)
+    # new_cam.set_widget_color(RED)
+    # self._next_cam_number += 1
+    # self._cameras.append(new_cam)
+
+    # # Resize and re-color previous camera
+    # if len(self._cameras) > 1:
+    #     self._cameras[-2].set_widget_color(BLACK)
+    #     self._cameras[-2].set_widget_focal_length(0.02)
 
     def _visualize_point_cloud(self, frame_pcd):
         points, colors = frame_pcd.get_points_and_colors()
-        cloud = ps.register_point_cloud(
-            "frame_pcd", points, radius=0.002, point_render_mode="quad"
-        )
+        cloud = ps.register_point_cloud("frame_pcd", points, point_render_mode="quad")
         cloud.add_color_quantity("colors", colors, enabled=True)
+        cloud.set_radius(0.01, relative=False)
 
     def _update_visualizer(self):
-        while ps._block_execution:
+        while PangolinoVisualizer.block_execution:
             ps.frame_tick()
-            if ps._play_mode:
+            if PangolinoVisualizer.play_mode:
                 break
-        ps._block_execution = not ps._block_execution
+        PangolinoVisualizer.block_execution = not PangolinoVisualizer.block_execution
 
     @staticmethod
-    def _gui_callback():
-        # START/STOP
-        if psim.Button("START/STOP"):
-            ps._play_mode = not ps._play_mode
-
-        # NEXT FRAME
-        if not ps._play_mode:
-            if psim.Button("NEXT FRAME"):
-                ps._block_execution = not ps._block_execution
-
-        # QUIT
-        if psim.Button("QUIT"):
-            print("Destroying Visualizer")
-            ps.unshow()
-            cv2.destroyAllWindows()
-            os._exit(0)
+    def quit():
+        ps.unshow()
+        cv2.destroyAllWindows()
+        print("Visualizer bye bye!")
