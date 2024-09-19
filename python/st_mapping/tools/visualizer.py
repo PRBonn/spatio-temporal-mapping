172571d08e022e81d521dc3aa2c68aa3d4799360
Luca Lobefaro <luca.lobefaro95@gmail.com>
2024-09-19 11:39:12 +0200

    Visualizer updated with state-of-the-art

--- python/st_mapping/tools/visualizer.py
+++ python/st_mapping/tools/visualizer.py
@@ -22,13 +22,28 @@
 import os
 from abc import ABC
 import polyscope as ps
-import polyscope.imgui as psim
+import polyscope.imgui as gui
 import cv2
 
 import numpy as np
 
+# Buttons names
+START_BUTTON = "START [SPACE]"
+PAUSE_BUTTON = "PAUSE [SPACE]"
+NEXT_FRAME_BUTTON = "NEXT FRAME [N]"
+QUIT_BUTTON = "QUIT [Q]"
+
+# Colors
 BACKGROUND_COLOR = [0.8470, 0.8588, 0.8863]
 CAMERA_COLOR = [1.0, 0.0, 0.0]
+
+# Size constants
+CLOUD_POINT_SIZE = 0.004
+POINTS_SIZE_STEP = 0.001
+POINTS_SIZE_MIN = 0.001
+POINTS_SIZE_MAX = 0.01
+INSTANCES_CENTERS_SIZE = 0.01
+LINES_SIZE = 0.003
 
 
 class StubVisualizer(ABC):
@@ -42,45 +57,8 @@ class StubVisualizer(ABC):
         pass
 
 
-def start_pause_callback():
-    button_name = "PAUSE" if PangolinoVisualizer.play_mode else "START"
-    if psim.Button(button_name) or psim.IsKeyPressed(psim.ImGuiKey_Space):
-        PangolinoVisualizer.play_mode = not PangolinoVisualizer.play_mode
-
-
-def next_frame_callback():
-    if psim.Button("NEXT FRAME") or psim.IsKeyPressed(psim.ImGuiKey_N):
-        PangolinoVisualizer.block_execution = not PangolinoVisualizer.block_execution
-
-
-def quit_callback():
-    if (
-        psim.Button("QUIT")
-        or psim.IsKeyPressed(psim.ImGuiKey_Escape)
-        or psim.IsKeyPressed(psim.ImGuiKey_Q)
-    ):
-        PangolinoVisualizer.quit()
-        os._exit(0)
-
-
-def main_gui_callback():
-    start_pause_callback()
-    if not PangolinoVisualizer.play_mode:
-        psim.SameLine()
-        next_frame_callback()
-    psim.Separator()
-    quit_callback()
-
-
-def ending_gui_callback():
-    quit_callback()
-
-
 class PangolinoVisualizer(StubVisualizer):
-    # Static parameteters
-    play_mode: bool = False
-    block_execution: bool = True
-
+    # --- PUBLIC INTERFACE ---------------------------------------------------------------
     def __init__(self):
         # Initialize visualizer
         self._initialize_visualizer()
@@ -89,10 +67,18 @@ class PangolinoVisualizer(StubVisualizer):
         # Initilize attributes
         self._camera_poses = []
 
+        # Initilize GUI controls and attributes
+        self._play_mode: bool = False
+        self._block_execution: bool = True
+        self._points_size: float = CLOUD_POINT_SIZE
+        self._points_transparency: float = 1.0
+
+        # Initialize visualizer
+        self._initialize_visualizer()
+
     def update(self, pose, rgb_img, frame_pcd):
         # Visualize image
-        cv2.imshow("Image Stream", rgb_img)
-        cv2.waitKey(1)
+        self._visualize_image(rgb_img)
 
         # Visualize camera pose
         self._update_camera_pose(pose)
@@ -105,19 +91,26 @@ class PangolinoVisualizer(StubVisualizer):
 
     def keep_running(self):
         cv2.destroyAllWindows()
-        PangolinoVisualizer.play_mode = False
-        PangolinoVisualizer.block_execution = True
-        ps.set_user_callback(ending_gui_callback)
+        self._play_mode = False
+        self._block_execution = True
+        ps.get_camera_view("camera").set_enabled(False)
+        ps.set_user_callback(self._keep_running_callback)
         ps.show()
 
+    # --- PRIVATE INTERFACE --------------------------------------------------------------
     def _initialize_visualizer(self):
         ps.set_program_name("Spatio-Temporal Mapping Visualizer")
         ps.init()
         ps.set_background_color(BACKGROUND_COLOR)
         ps.set_verbosity(0)
         ps.set_ground_plane_mode("none")
-        ps.set_user_callback(main_gui_callback)
+        ps.set_user_callback(self._main_gui_callback)
         ps.set_build_default_gui_panels(False)
+        cv2.namedWindow("Camera Stream")
+
+    def _visualize_image(self, img):
+        cv2.imshow("Camera Stream", img)
+        cv2.waitKey(1)
 
     def _update_camera_pose(self, pose):
         self._camera_poses.append(pose)
@@ -130,19 +123,98 @@ class PangolinoVisualizer(StubVisualizer):
 
     def _visualize_point_cloud(self, frame_pcd):
         points, colors = frame_pcd.get_points_and_colors()
-        cloud = ps.register_point_cloud("frame_pcd", points, point_render_mode="quad")
+        cloud = ps.register_point_cloud(
+            "map_pcd",
+            points,
+            point_render_mode="quad",
+            transparency=self._points_transparency,
+        )
         cloud.add_color_quantity("colors", colors, enabled=True)
-        cloud.set_radius(0.005, relative=False)
+        cloud.set_radius(self._points_size, relative=False)
 
     def _update_visualizer(self):
-        while PangolinoVisualizer.block_execution:
+        while self._block_execution:
             ps.frame_tick()
-            if PangolinoVisualizer.play_mode:
+            if self._play_mode:
                 break
-        PangolinoVisualizer.block_execution = not PangolinoVisualizer.block_execution
+        self._block_execution = not self._block_execution
 
-    @staticmethod
-    def quit():
-        ps.unshow()
-        cv2.destroyAllWindows()
-        print("Visualizer bye bye!")
+    # --- GUI Callbacks ------------------------------------------------------------------
+    def _start_pause_callback(self):
+        button_name = PAUSE_BUTTON if self._play_mode else START_BUTTON
+        if gui.Button(button_name) or gui.IsKeyPressed(gui.ImGuiKey_Space):
+            self._play_mode = not self._play_mode
+
+    def _next_frame_callback(self):
+        if gui.Button(NEXT_FRAME_BUTTON) or gui.IsKeyPressed(gui.ImGuiKey_N):
+            self._block_execution = not self._block_execution
+
+    def _points_size_callback(self):
+        key_changed = False
+        if gui.IsKeyPressed(gui.ImGuiKey_Minus):
+            self._points_size = max(
+                POINTS_SIZE_MIN, self._points_size - POINTS_SIZE_STEP
+            )
+            key_changed = True
+        if gui.IsKeyPressed(gui.ImGuiKey_Equal):
+            self._points_size = min(
+                POINTS_SIZE_MAX, self._points_size + POINTS_SIZE_STEP
+            )
+            key_changed = True
+        changed, self._points_size = gui.SliderFloat(
+            "Points Size",
+            self._points_size,
+            v_min=POINTS_SIZE_MIN,
+            v_max=POINTS_SIZE_MAX,
+        )
+        if changed or key_changed:
+            ps.get_point_cloud("map_pcd").set_radius(self._points_size, relative=False)
+
+    def _points_transparency_callback(self):
+        changed, self._points_transparency = gui.SliderFloat(
+            "Points Transparency",
+            self._points_transparency,
+            v_min=0,
+            v_max=1.0,
+        )
+        if changed:
+            ps.get_point_cloud("map_pcd").set_transparency(self._points_transparency)
+
+    def _quit_callback(self):
+        posX = (
+            gui.GetCursorPosX()
+            + gui.GetColumnWidth()
+            - gui.CalcTextSize(QUIT_BUTTON)[0]
+            - gui.GetScrollX()
+            - gui.ImGuiStyleVar_ItemSpacing
+        )
+        gui.SetCursorPosX(posX)
+        if (
+            gui.Button(QUIT_BUTTON)
+            or gui.IsKeyPressed(gui.ImGuiKey_Escape)
+            or gui.IsKeyPressed(gui.ImGuiKey_Q)
+        ):
+            print("Visualizer Bye Bye!")
+            ps.unshow()
+            cv2.destroyAllWindows()
+            os._exit(0)
+
+    def _scene_options_callback(self):
+        gui.TextUnformatted("Scene Options:")
+        self._points_size_callback()
+        self._points_transparency_callback()
+
+    def _main_gui_callback(self):
+        self._start_pause_callback()
+        if not self._play_mode:
+            gui.SameLine()
+            self._next_frame_callback()
+        gui.Separator()
+        self._scene_options_callback()
+        gui.Separator()
+        self._quit_callback()
+
+    def _keep_running_callback(self):
+        self._scene_options_callback()
+        gui.Separator()
+        self._quit_callback()
